8c1d8bb1014c759dd82f345c7b785a624db6ed68
Petersen, Anya <Anya.Petersen@nrel.gov>
2018-09-06 10:04:53 -0600

    updated artifact pattern and use full paths to ruby and oss, although the prior syntax did run.

--- ci/appveyor/build_release.cmd
+++ ci/appveyor/build_release.cmd
@@ -1,1 +1,1 @@
-ruby bin/openstudio_meta install_gems -e /e/openstudio-server-build
+C:\Ruby%RUBY_VERSION%\bin\ruby C:\projects\openstudio-server\bin\openstudio_meta install_gems -e /e/openstudio-server-build
